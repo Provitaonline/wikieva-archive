d51c5a313662a04862d633a911c5bfb183be837d
Ricardo Morin <morinricardo@gmail.com>
2022-06-11 11:06:20 -0700

    Initial commit

---
+++ web/images/apidf90.php
@@ -0,0 +1,11 @@
+<?xml version="1.0"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="es">
+		<id>http://wikieva.org.ve/api.php?hidebots=1&amp;days=7&amp;limit=50&amp;action=feedrecentchanges&amp;feedformat=atom</id>
+		<title>WikiEVA  - Cambios recientes [es]</title>
+		<link rel="self" type="application/atom+xml" href="http://wikieva.org.ve/api.php?hidebots=1&amp;days=7&amp;limit=50&amp;action=feedrecentchanges&amp;feedformat=atom"/>
+		<link rel="alternate" type="text/html" href="http://wikieva.org.ve/index.php/Especial:CambiosRecientes"/>
+		<updated>2021-06-26T02:18:19Z</updated>
+		<subtitle>Realiza un seguimiento de los cambios más recientes en el wiki con este canal.</subtitle>
+		<generator>MediaWiki 1.26.3</generator>
+
+	</feed>
